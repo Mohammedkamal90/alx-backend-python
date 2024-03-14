8f3c8b23800cba41be15ad5d627d5b83839e1487
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-14 08:21:15 -0700

    task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Concatenates two strings together.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string of str1 and str2.
+"""
+
+
+
+def concat(str1: str, str2: str) -> str:
+
+    return str1 + str2
