e992848877f93318e2010a75fc14ffff1e0fa22d
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-14 09:00:50 -0700

    task101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-"""give parameters, return values, add type
+"""give parameter, return values, add type
 annotations to function
+
+Hint: look into TypeVar
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
 """
 from typing import TypeVar, Mapping, Any, Union
 
