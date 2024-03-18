538f613b29421abe1c357a63a377fc47f6c9132b
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-18 08:25:09 -0700

    task4

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-""" async basics """
+"""async basic"""
 
-import asyncio
-from typing import Callable
+from asyncio import Task, create_task
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
