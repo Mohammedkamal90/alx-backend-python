ce87746cc0c1bc8c2f84a9f69ec90a40b3665be3
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-18 08:08:49 -0700

    task2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,6 +6,7 @@ import time
 from typing import List
 from random import uniform
 
+
 async def wait_random(max_delay: int = 10) -> float:
     delay = uniform(0, max_delay)
     await asyncio.sleep(delay)
