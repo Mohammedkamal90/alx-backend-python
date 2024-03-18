08f378f3dde4f54f8999c3bec81e8e2529a8c041
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-03-18 08:11:13 -0700

    task2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,14 +7,8 @@ from typing import List
 from random import uniform
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    delay = uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
 
 def measure_time(n: int, max_delay: int) -> float:
     """measure runtime"""
